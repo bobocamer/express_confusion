9e361b0b2b1bac681a4ce360024964f21522ce69
Boaz Njintchoum <boaz.na@gmail.com>
2018-06-19 15:43:14 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '854678924721266',
+        clientSecret: 'eb3144074a50f50aa07cde6bb9d7f22a'
+    }
 }
